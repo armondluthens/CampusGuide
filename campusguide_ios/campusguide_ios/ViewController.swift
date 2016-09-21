200d2795ae1467f6ad467508ed39599832174dd7
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-09-21 12:56:37 -0500

    blank ios app

---
+++ campusguide_ios/campusguide_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  campusguide_ios
+//
+//  Created by Stephanie Smith on 9/21/16.
+//  Copyright © 2016 FantasticFour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
