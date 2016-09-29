06fe54604de8a17fd374c521eeb181686b9b5910
Armond Luthens <armondluthens@gmail.com>
2016-09-29 13:19:05 -0500

    updated app

--- BlankProjectTest/BlankProjectTest/ViewController.swift
+++ BlankProjectTest/BlankProjectTest/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, identifier: "Estimotes")
+    
+    let colors = [
+        //purple beacon
+        62098: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
+        
+        //green beacon
+        73: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self;
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startRangingBeaconsInRegion(region)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon
+            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
+        }
+    }
+    
 
 }
 
