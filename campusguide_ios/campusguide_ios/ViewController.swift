08d187d997e02e1b3426bfba29c529a85a49f5af
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-10-03 21:52:46 -0500

    tested basic flora functionality and beginning wearable prototype code on arduino

--- campusguide_ios/campusguide_ios/ViewController.swift
+++ campusguide_ios/campusguide_ios/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+//UITextFieldDelegate
+// adopting protocol UITextFieldDelegate ...make ViewController the text box's delegate
+class ViewController: UIViewController,UITextFieldDelegate  {
+    // MARK: Properties
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    // keep but am not overriding right now
 
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+    // MARK: UITextFieldDelegate
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+        // returning the value true indicates that the text field should respond to the user pressing the Return key by dismissing the keyboard.
+    }
+    
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+
+    
+
+    
+    // MARK: Actions
+
+    @IBAction func etDefaultLabelText(sender: UIButton) {
+        
+        
+        mealNameLabel.text = "Default Text"
+        
+
+    }
 
 }
 
