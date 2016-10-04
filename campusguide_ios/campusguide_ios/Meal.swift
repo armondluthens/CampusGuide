3335b23e6207bbf15529d64866e981579282f75d
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-10-04 11:55:53 -0500

    refined sonar and added bluetooth

---
+++ campusguide_ios/campusguide_ios/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  campusguide_ios
+//
+//  Created by Stephanie Smith on 10/4/16.
+//  Copyright © 2016 FantasticFour. All rights reserved.
+//
+
+import Cocoa
+
+class Meal: NSObject {
+
+}
--- wearable/prototype/prototpye/prototpye.ino
+++ wearable/prototype/prototpye/prototpye.ino
@@ -5,29 +5,48 @@ const float distance_threshold = 2.0 * 39.3701;
 const int bluetooth_pin; 
 Messages message;
 float vi = 3.3/512;
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 // calculate distance from analog input: http://www.maxbotix.com/articles/032.htm
 
 void setup() {
   Serial.begin(9600);
-
+  // for bluetooth test
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  if(read_distance() <= distance_threshold){
+   int distance = read_distance();
+  if(distance <= distance_threshold && distance > 0){
       Serial.println("within distance");
   }
-
+  blue_tooth_test();
+}
+void blue_tooth_test(){
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    switch(inChar) {
+      case '1':
+        digitalWrite(13, HIGH);
+      break;
+      case '0':
+        digitalWrite(13, LOW);
+      break;
+    }
+    Serial.println(inChar);
+  }
 }
 int read_distance(){
-   int distance_sensor = -1;
+   float distance_sensor = -1;
      // average out over 8 readings   
-//   int i = 0;
-//   for (i=0; i<8; i++) {
-//     distance_sensor += analogRead(0);
-//     delay(50);
-//   }
-//   distance_sensor /= 8;
+   int i = 0;
+   for (i=0; i<8; i++) {
+     distance_sensor += analogRead(0);
+     delay(50);
+   }
+   distance_sensor /= 8;
    distance_sensor = analogRead(0)/vi;
+   Serial.println(distance_sensor);
 }
 void vibrate_motors(){
   
