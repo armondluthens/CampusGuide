53b93efe51b691a95bfb27cf698925db3339e4da
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-12-03 11:59:01 -0600

    bluetooth works to best of knowledge

--- BluetoothTest/BluetoothTest/BluetoothDefinitions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BluetoothDefinitions.swift
-//  BluetoothTest
-//
-//  Created by Stephanie Smith on 12/1/16.
-//  Copyright © 2016 FantasticFour. All rights reserved.
-//
-
-import Foundation
