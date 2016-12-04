e7f469d554e334d54d6ef313793b6b240fb402f2
Stephanie Smith <stephanie-k-smith@uiowa.edu>
2016-12-04 12:15:39 -0600

    finished speech recognition with month/weekday commands

---
+++ Test2/Test2/Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  Bridging-Header.h
+//  Test2
+//
+//  Created by Stephanie Smith on 12/3/16.
+//  Copyright © 2016 armondluthens. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEAcousticModel.h>
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h>
+
+#endif /* Bridging_Header_h */
